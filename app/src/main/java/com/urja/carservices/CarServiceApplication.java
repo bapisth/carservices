2104ce740f94295ec1256a7581c9e37b562ecd93
bapisth <hemendra7011@rediffmail.com>
2016-08-14 19:04:18 +0530

    Code Optimized!!

--- app/src/main/java/com/urja/carservices/CarServiceApplication.java
+++ app/src/main/java/com/urja/carservices/CarServiceApplication.java
@@ -1,17 +1,23 @@
 package com.urja.carservices;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.util.DrawerImageLoader;
+import com.urja.carservices.utils.CurrentLoggedInUser;
 
 /**
  * Created by BAPI1 on 8/14/2016.
  */
 
 public class CarServiceApplication extends Application {
+    private static final String TAG = CarServiceApplication.class.getSimpleName();
+    FirebaseAuth mAuth;
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d(TAG, "onCreate: ");
     }
 }
